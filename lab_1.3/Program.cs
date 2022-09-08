24750e11dcd378c0b1aeeb892bd41c528b8c60f8
YuEgor <rezervdesu15@gmail.com>
2022-09-08 21:02:09 +0800

    Добавьте файлы проекта.

---
+++ lab_1.3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace FirstApplication
+{
+
+    public class Logic
+    {
+        public static string [] Sort(string Sentense)
+        {
+            string[] wordsArray = Sentense.Split(' ', ',', '.');
+            int[] wordsLenghtArray = new int[wordsArray.Length];
+            for (int i = 0; i < wordsLenghtArray.Length; i++)
+            {
+                wordsLenghtArray[i] = wordsArray[i].Length;
+            }
+
+            Array.Sort(wordsLenghtArray, wordsArray);
+            Array.Reverse(wordsArray);
+
+            return wordsArray;
+        }
+    }
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //НАЧАЛО взаимодейтсвия с пользователем
+            Console.WriteLine("Введите ваше предложение:");
+            var yourSentense = Console.ReadLine();
+            //КОНЕЦ взаимодействия с пользователем
+
+            //НАЧАЛО логики
+            string [] outArray = Logic.Sort(yourSentense);
+            //КОНЕЦ логики
+
+            //НАЧАЛО взаимодейтсвия с пользователем
+            Console.WriteLine("Ваше предложение было отсортированно в порядке убывания длин слов:");
+            foreach(string Words in outArray)
+                Console.WriteLine(Words);
+            Console.ReadLine();
+            //КОНЕЦ взаимодейтсвия с пользователем
+        }
+    }
+}
+
---
+++ lab_1.3Tests/LogicTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void sortTest3Words()
+        {
+            
+            string yourSentence = "n nn nnn";
+            string[] checkArray = { "nnn", "nn", "n" };
+            
+
+            string [] outArray = Logic.Sort(yourSentence);
+                
+            Assert.AreEqual(checkArray[0], outArray[0]);
+            Assert.AreEqual(checkArray[1], outArray[1]);
+            Assert.AreEqual(checkArray[2], outArray[2]);
+
+
+        }
+
+        [TestMethod()]
+        public void sortTest4Words()
+        {
+
+            string yourSentence = "n nnnn nnn nn";
+            string[] checkArray = { "nnnn", "nnn", "nn", "n" };
+
+
+            string[] outArray = Logic.Sort(yourSentence);
+
+            Assert.AreEqual(checkArray[0], outArray[0]);
+            Assert.AreEqual(checkArray[1], outArray[1]);
+            Assert.AreEqual(checkArray[2], outArray[2]);
+            Assert.AreEqual(checkArray[3], outArray[3]);
+
+
+        }
+
+        [TestMethod()]
+        public void sortTest5Words()
+        {
+
+            string yourSentence = "n nnnn nnn nn nnnnn";
+            string[] checkArray = { "nnnnn", "nnnn", "nnn", "nn", "n" };
+
+
+            string[] outArray = Logic.Sort(yourSentence);
+
+            Assert.AreEqual(checkArray[0], outArray[0]);
+            Assert.AreEqual(checkArray[1], outArray[1]);
+            Assert.AreEqual(checkArray[2], outArray[2]);
+            Assert.AreEqual(checkArray[3], outArray[3]);
+            Assert.AreEqual(checkArray[4], outArray[4]);
+
+
+        }
+    }
+}
